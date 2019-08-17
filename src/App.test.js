d7c405587577ea78ff908ca686fac1f01f55de7c
Nobuyuki Mukai <ycuobu@gmail.com>
2019-08-18 01:16:53 +0900

    component

--- src/App.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import MarkdownTextBox from './MarkdownTextBox';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<MarkdownTextBox />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
--- src/index.js
+++ src/index.js
@@ -1,12 +1,112 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import MarkdownTextBox from './MarkdownTextBox';
-import * as serviceWorker from './serviceWorker';
+import React, {Component} from 'react';
+import Compiler from '3jsc';
 
-ReactDOM.render(<div><MarkdownTextBox /></div>, document.getElementById('root'));
+class MarkdownTextBox extends Component{
+  constructor(props)
+  {
+    super(props);
+    this.descriptionref=React.createRef();
+    this.textarearef=React.createRef();
+    this.state={markdownvalue: this.getInitialValue(), focus:true};
+    if(this.props.compiler==null){this.compiler=new Compiler()}else{this.compiler=this.props.compiler}
+  }
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  getInitialValue()
+  {
+      let propvalue=this.props.value;
+      if(propvalue!=null)
+      {
+          return propvalue;
+      }
+      else
+      {
+          return "";
+      }
+  }
+
+  onChangeHandler(e)
+  {
+    this.setState({markdownvalue:e.target.value})
+  }
+
+  onBlurHandler(e)
+  {
+    this.setState({focus:false})
+  }
+  onFocusHandler(e)
+  {
+    this.setState({focus:true})
+  }
+
+
+  getTopLevelStyle()
+  {
+      if(this.props.style==null)
+      {
+          return {position:"relative",
+              width:"200px",
+              height:"50px"};
+      }else
+      {
+          return this.props.style;
+      }
+
+  }
+
+  getDescriptionStyle()
+  {
+    let display="block";
+    if(this.state.focus){display="none"};
+    return {position:"relative",
+            display: display,
+            width: "100%",
+            height: "100%"
+          }
+  }
+
+  getTextAreaStyle()
+  {
+    let display="none";
+    if(this.state.focus){display="block"};
+    return {position:"relative",
+            display: display,
+            width: "100%",
+            height: "100%"
+            }
+  }
+
+  getCalculatedText(text)
+  {
+      let re = new RegExp(/\$\(([^$()]*)\)/g);
+      return text.replace(re,(match, capture)=>{return this.compiler.calc(capture)})
+  }
+
+  safeExec(callback,e)
+  {
+      if(typeof(callback)=='function')
+      {
+          callback(e)
+      }
+  }
+
+  render()
+  {
+    return (
+        <div style={this.getTopLevelStyle()}>
+          <div style={this.getDescriptionStyle()}
+               onClick={e=>this.onFocusHandler(e)}
+               ref={e=>{this.descriptionref=e}}>{this.getCalculatedText(this.state.markdownvalue)} </div>
+          <textarea style={this.getTextAreaStyle()}
+                    onKeyDown={e=>{this.safeExec(this.props.onKeyDown,e)}}
+                    onChange={e=>{this.onChangeHandler(e);this.safeExec(this.props.onChange,e)}}
+                    onBlur={e=>{this.onBlurHandler(e)}}
+                    onFocus={e=>{this.onFocusHandler(e)}}
+                    value={this.state.markdownvalue}
+                    ref={e=>{this.textarearef=e}}></textarea>
+        </div>
+    );
+  }
+
+}
+
+export default MarkdownTextBox;
